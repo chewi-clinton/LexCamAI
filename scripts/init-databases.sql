887212eb33949563f29adffb7f1ef4cd8544066c
chewi-clinton <suhchenwi48@gmail.com>
2026-05-12 06:59:08 +0100

    initialize the monorepo structure

---
+++ scripts/init-databases.sql
@@ -0,0 +1,13 @@
+-- LexCam PostgreSQL initialization
+-- Runs automatically on first container start
+
+CREATE DATABASE lexcam_users;
+CREATE DATABASE lexcam_lawyers;
+CREATE DATABASE lexcam_documents;
+CREATE DATABASE lexcam_payments;
+CREATE DATABASE lexcam_notif;
+CREATE DATABASE lexcam_feedback;
+CREATE DATABASE lexcam_admin;
+CREATE DATABASE lexcam_scraping;
+CREATE DATABASE lexcam_knowledge;
+CREATE DATABASE lexcam_rag_sessions;
